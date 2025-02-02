81f02f37d4696c03e3323927bd9347f0519f89b7
Tin Do <dothanhtin003@gmail.com>
2025-02-02 14:31:24 -0500

    ugly fe

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,10 +1,10 @@
-import Landing from '@/app/components/landing'
-import Navbar from './components/navbar'
+import Landing from "./components/landing";
+import Navbar from "./components/navbar";
 export default function Home() {
-  return (
-    <main className="bg-gradient-to-b from-green-100 to-green-200 sm:h-full lg:h-screen">
-      <Navbar/>
-      <Landing/>
-    </main>
-  )
+    return (
+        <main className="bg-gradient-to-b from-green-100 to-green-200 sm:h-full lg:h-screen">
+            <Navbar />
+            <Landing />
+        </main>
+    );
 }
